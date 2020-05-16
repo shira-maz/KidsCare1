49be8ba39bb3fd1f2dc60fed94da3fa5619242be
shira-maz <SHIRAMAZARI50@GMAIL.COM>
2020-05-16 16:12:11 +0300

    update

--- index.php
+++ index.php
@@ -48,8 +48,5 @@
     <script src="app-assets/vendors/js/vendors.min.js" type="text/javascript"></script>
     <script src="app-assets/vendors/js/forms/validation/jqBootstrapValidation.js" type="text/javascript"></script>
     <script src="app-assets/js/core/app-menu.js" type="text/javascript"></script>
-
-
 </body>
-
 </html>
