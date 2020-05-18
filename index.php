2a6ae95eb7117359170396b7569f10ed4edd80a5
shira-maz <SHIRAMAZARI50@GMAIL.COM>
2020-05-18 15:45:36 +0300

    first commit

--- index.php
+++ index.php
@@ -48,9 +48,9 @@
         </div>
     </div>
     <script src="app-assets/vendors/js/vendors.min.js" type="text/javascript"></script>
+    <script src="app-assets/vendors/js/forms/validation/jqBootstrapValidation.js" type="text/javascript"></script>
     <script src="app-assets/js/core/app-menu.js" type="text/javascript"></script>
-    <script src="app-assets/js/core/app.js" type="text/javascript"></script>
-    <script src="app-assets/js/scripts/forms/form-login-register.js" type="text/javascript"></script>
+
 
 </body>
 
